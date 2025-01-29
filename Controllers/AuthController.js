a453a3587eb72ab2d800531ccbc697b763f2e894
Soukaina Mourabit <soukainamourabit9@gmail.com>
2025-01-29 18:25:32 +0100

    adding error messages + redirections

--- Controllers/AuthController.js
+++ Controllers/AuthController.js
@@ -10,7 +10,11 @@ exports.signUp = async (req, res) => {
     // Check if user already exists
     const existingUser = await User.findOne({ email });
     if (existingUser) {
-      return res.status(400).send('User already exists');
+      return res.status(400).render('auth/inscription', {
+        title: 'Inscription', // Pass title
+        message: 'User already exists',
+        messageType: 'danger',
+      });
     }
 
     // Hash password
@@ -26,12 +30,21 @@ exports.signUp = async (req, res) => {
     // Save user to MongoDB
     await newUser.save();
 
-    res.status(201).send('User created successfully');
+    res.status(201).render('pages/documents', {
+      title: 'documents', // Pass title
+  
+    });
   } catch (err) {
     console.error(err);
-    res.status(500).send('Server error');
+    res.status(500).render('auth/inscription', {
+      title: 'Inscription', // Pass title
+      message: 'Server error. Please try again later.',
+      messageType: 'danger',
+    });
   }
 };
+
+
 
 
 // Login Function
@@ -42,13 +55,21 @@ exports.login = async (req, res) => {
     // Find user by email
     const user = await User.findOne({ email });
     if (!user) {
-      return res.status(400).send('Invalid credentials');
+      return res.status(400).render('auth/login', {
+       title: 'connexion', // Pass title
+       message: 'Invalid credentials',
+       messageType: 'danger',
+     });      
     }
 
     // Compare passwords
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
-      return res.status(400).send('Invalid credentials');
+      return res.status(400).render('auth/login', {
+       title: 'connexion', // Pass title
+       message: 'Invalid credentials',
+       messageType: 'danger',
+     });
     }
 
     // Generate JWT token
@@ -56,10 +77,19 @@ exports.login = async (req, res) => {
       expiresIn: '1h',
     });
 
-    res.cookie('token', token, { httpOnly: true }).send('Logged in successfully');
+    res.cookie('token', token, { httpOnly: true })
+
+    res.status(201).render('pages/documents', {
+      title: 'documents', // Pass title
+    });
+
+
   } catch (err) {
-    console.error(err);
-    res.status(500).send('Server error');
+    return res.status(500).render('auth/login', {
+      title: 'connexion', // Pass title
+      message: 'Server error',
+      messageType: 'danger',
+    });
   }
 };
 
