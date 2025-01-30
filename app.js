9bb90902aac508ccca1e6abab204bbeace3b03a0
samenthalo <ortega.samantha81400@gmail.com>
2025-01-30 12:08:39 +0100

    delete cookie

--- app.js
+++ app.js
@@ -2,14 +2,12 @@ const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const routes = require('./routes/index');
 const path = require('path');
-const cookieParser = require('cookie-parser');
 
 const app = express();
 
 /* Config EJS */
 app.set('view engine', 'ejs');
 app.set('layout', 'layout');
-app.use(cookieParser());
 
 app.use(expressLayouts);
 app.use(express.json()); // for parsing application/json
